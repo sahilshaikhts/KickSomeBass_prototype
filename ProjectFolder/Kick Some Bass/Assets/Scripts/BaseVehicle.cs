1fdbb8dbe88b72792cf7cba158eba7a3cc387669
Yash Chamria <59587121+Yashchamria@users.noreply.github.com>
2021-09-24 12:45:22 -0400

    Initial Fighting commit

--- ProjectFolder/Kick Some Bass/Assets/Scripts/BaseVehicle.cs
+++ ProjectFolder/Kick Some Bass/Assets/Scripts/BaseVehicle.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MyInterfaces;
-
 public class BaseVehicle : MonoBehaviour
 {
     [SerializeField] protected GameObject m_seat;
